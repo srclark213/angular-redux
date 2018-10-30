3cde3779d185322854dba91c0bebfcd30b7277cd
Steve Clark <SClark@ethosgroup.com>
2018-10-29 19:09:50 -0500

    built initial redux boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from './store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-redux';
+
+  constructor(private store: StoreService) {
+  }
+
+  ngOnInit() {
+    this.store.getState(s => s.title).subscribe((data) => this.title = data);
+  }
+
+  changeTitle() {
+    this.store.dispatch({type: 'CHANGE_TITLE', value: 'changed'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreService } from './store.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
